11150dfc7799996da290220267eb06238a42ebf1
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-26 14:51:13 +0900

    add: ホーム戻るリンク

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,9 @@
+import { BackToHome } from "../components/indexcomponents";
 const Login = () => {
   return (
     <div>
       <h1>ログインページ</h1>
+      <BackToHome />
     </div>
   );
 };
--- frontend/src/pages/NewAccount.tsx
+++ frontend/src/pages/NewAccount.tsx
@@ -1,7 +1,9 @@
+import { BackToHome } from "../components/indexcomponents";
 const NewAccount = () => {
   return (
     <div>
       <h1>新規登録ページ</h1>
+      <BackToHome />
     </div>
   );
 };
