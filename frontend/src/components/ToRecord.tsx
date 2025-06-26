69c18bb723241e599540a25bf0330c7510ebd7e8
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-26 15:06:05 +0900

    add: マイページボタンたち（プレゼン、ランキング、記録へ遷移）

---
+++ frontend/src/components/ToRecord.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+const ToRecord = () => {
+  return (
+    <div>
+      <Link to={"/record"}>記録をみる</Link>
+    </div>
+  );
+};
+
+export default ToRecord;
--- frontend/src/components/indexComponents.tsx
+++ frontend/src/components/indexComponents.tsx
@@ -1,2 +1,5 @@
 export { default as BackToHome } from "./BackToHome";
 export { default as BackToMyPage } from "./BackToMyPage";
+export { default as ToPresentation } from "./ToPresentation";
+export { default as ToRanking } from "./ToRanking";
+export { default as ToRecord } from "./ToRecord";
