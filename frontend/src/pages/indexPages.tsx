c78899ed6ed293afbc9fd407a6098b01d0b4773e
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-26 14:12:50 +0900

    add: Pagesのindexファイル

---
+++ frontend/src/pages/indexPages.tsx
@@ -0,0 +1,6 @@
+export { default as Login } from "./Login";
+export { default as Evaluation } from "./Evaluation";
+export { default as NewAccount } from "./NewAccount";
+export { default as Presentation } from "./Presentation";
+export { default as Ranking } from "./Ranking";
+export { default as UserHome } from "./UserHome";
