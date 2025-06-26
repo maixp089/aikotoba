d2a0fcbee1f113cc7b2302a135b6db6e1e954438
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-26 14:46:17 +0900

    add: ログイン・新規登録ページ遷移

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div>
       <h1>ホームページ</h1>
+      <Link to={"/login"}>ログイン</Link>
+      <br />
+      <br />
+      <Link to={"/newaccount"}>新規登録</Link>
     </div>
   );
 };
