af7b34fda7c0c4acbdc810c197a4ab7773c76187
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-26 15:26:54 +0900

    update: 文言変更

--- frontend/src/components/ToRecord.tsx
+++ frontend/src/components/ToRecord.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const ToRecord = () => {
   return (
     <div>
-      <Link to={"/record"}>記録をみる</Link>
+      <Link to={"/record"}>記録一覧ページへ</Link>
     </div>
   );
 };
