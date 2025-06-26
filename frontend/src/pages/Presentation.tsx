3b0c144084b28c05ebde9b021b73c6545d16f58c
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-26 15:24:44 +0900

    add: 評価ページ遷移

--- frontend/src/pages/Presentation.tsx
+++ frontend/src/pages/Presentation.tsx
@@ -1,10 +1,11 @@
-import { BackToMyPage } from "../components";
+import { BackToMyPage, ToEvaluation } from "../components";
 
 const Presentation = () => {
   return (
     <div>
       <h1>プレゼンページ</h1>
       <BackToMyPage />
+      <ToEvaluation />
     </div>
   );
 };
--- frontend/src/pages/Record.tsx
+++ frontend/src/pages/Record.tsx
@@ -1,10 +1,11 @@
-import { BackToMyPage } from "../components";
+import { BackToMyPage, ToEvaluation } from "../components";
 
 const Record = () => {
   return (
     <div>
       <h1>記録ページ</h1>
       <BackToMyPage />
+      <ToEvaluation />
     </div>
   );
 };
