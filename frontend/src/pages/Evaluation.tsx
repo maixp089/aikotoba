5a6e28f5e6cc5c1c189e661ee6ff2e76e568e1b6
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-26 15:13:20 +0900

    add: マイページ、記録ページ遷移リンク

--- frontend/src/pages/Evaluation.tsx
+++ frontend/src/pages/Evaluation.tsx
@@ -1,7 +1,12 @@
+import BackToMyPage from "../components/BackToMyPage";
+import ToRecord from "../components/ToRecord";
+
 const Evaluation = () => {
   return (
     <div>
       <h1>講評ページ</h1>
+      <BackToMyPage />
+      <ToRecord />
     </div>
   );
 };
