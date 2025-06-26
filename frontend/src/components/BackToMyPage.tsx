a79272efcbdfc4f1fc957453844d5357508ca33c
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-26 15:00:52 +0900

    update: パス修正

--- frontend/src/components/BackToMyPage.tsx
+++ frontend/src/components/BackToMyPage.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const BackToMyPage = () => {
   return (
     <div>
-      <Link to={"/userhome"}>マイページへ戻る</Link>
+      <Link to={"/mypage"}>マイページへ戻る</Link>
     </div>
   );
 };
