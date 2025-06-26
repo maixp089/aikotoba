324b26f10b136be2cdded71b8ba0a321d34e1358
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-26 15:21:35 +0900

    add: 各機能ページ遷移

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,7 +1,13 @@
+import { BackToHome, ToPresentation, ToRanking, ToRecord } from "../components";
+
 const UserHome = () => {
   return (
     <div>
       <h1>ユーザホーム画面ページ</h1>
+      <BackToHome />
+      <ToPresentation />
+      <ToRecord />
+      <ToRanking />
     </div>
   );
 };
