715cbb04b95679411fa8b2647fb4e6734e19b77d
えがしら0919 <@example.com>
2025-06-27 12:34:09 +0900

    Add: 新規ユーザー登録画面/front,fetch追記

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,27 @@
 # app/main.py
-
-from fastapi import FastAPI
-from app.db import models
-from app.db.database import engine
-
-# テーブルを作成（初回起動時に）
-models.Base.metadata.create_all(bind=engine)
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.db import models, schemas, database
 
 app = FastAPI()
 
-# 動作確認用のルート
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI + Docker!"}
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/users", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.user_name == user.user_name).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="ユーザー名は既に使われています")
+    new_user = models.User(
+        user_name=user.user_name,
+        age=user.age
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
