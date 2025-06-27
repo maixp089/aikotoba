e89cba3f221cd0689a520baa22566a3f77f1ad42
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-27 14:43:18 +0900

    update: ボタン装飾by tailwind

--- frontend/src/components/BackToHome.tsx
+++ frontend/src/components/BackToHome.tsx
@@ -9,7 +9,12 @@ const BackToHome = () => {
 
   return (
     <div>
-      <button onClick={onClick}>ホームへ</button>
+      <button
+        onClick={onClick}
+        className="flex h-9 items-center justify-center rounded-full bg-gradient-to-b from-blue-400 from-50% to-blue-500 to-50% px-3 text-blue-50 hover:from-blue-500 hover:to-blue-600 active:from-blue-600 active:to-blue-700"
+      >
+        ホームへ
+      </button>
     </div>
   );
 };
--- frontend/src/components/ToPresentation.tsx
+++ frontend/src/components/ToPresentation.tsx
@@ -7,7 +7,12 @@ const ToPresentation = () => {
   };
   return (
     <div>
-      <button onClick={onClick}>プレゼン練習をする</button>
+      <button
+        onClick={onClick}
+        className="flex h-9 items-center justify-center rounded-full bg-gradient-to-b from-blue-400 from-50% to-blue-500 to-50% px-3 text-blue-50 hover:from-blue-500 hover:to-blue-600 active:from-blue-600 active:to-blue-700"
+      >
+        プレゼン練習をする
+      </button>
     </div>
   );
 };
