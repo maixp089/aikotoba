970467b57e44a459fba5a6e31bb2b232c688927b
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-27 17:00:53 +0900

    add: コンポーネント間スペース

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,9 +3,11 @@ import { ToLogin, ToNewAccount, Layout } from "../components";
 const Home = () => {
   return (
     <Layout>
-      <h1 className=" text-5xl text-center ">ホームページ</h1>
-      <ToLogin />
-      <ToNewAccount />
+      <div className="space-y-10">
+        <h1 className=" text-5xl text-center ">ホームページ</h1>
+        <ToLogin />
+        <ToNewAccount />
+      </div>
     </Layout>
   );
 };
