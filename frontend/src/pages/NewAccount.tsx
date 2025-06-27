01bc50e45b523317a73bc1a23eefd2bc203d8ec2
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-27 17:00:29 +0900

    add: 画像登録欄（仮）

--- frontend/src/pages/NewAccount.tsx
+++ frontend/src/pages/NewAccount.tsx
@@ -24,14 +24,26 @@ const NewAccount = () => {
             </label>
             <div className="mt-1">
               <input
-                type="text"
+                type="Number"
                 placeholder="数字だけ入力しましょう    例：４"
                 className="border border-gray-300 focus:outline-blue-400 rounded w-full h-10 p-3 pt-5 text-sm"
               ></input>
             </div>
           </div>
+          <div className="relative mx-auto w-full max-w-xs">
+            <label className="absolute bg-white px-1 text-sm block text-gray-800 bottom-7 left-2">
+              画像登録
+            </label>
+            <div className="mt-1">
+              <input
+                type="text"
+                placeholder="アイコン画像登録"
+                className="border border-gray-300 focus:outline-blue-400 rounded w-full h-10 p-3 pt-5 text-sm"
+              ></input>
+            </div>
+          </div>
           <button className="p-2 rounded bg-green-500 hover:bg-green-600 text-white border-gray-700 mx-1">
-            登録する
+            登録する このボタン押したらマイページ飛ぶ
           </button>
         </div>
       </div>
