3d58d5dacce47dfbc268fbf65dbffbeb50760e75
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-27 10:42:16 +0900

    update: eslint 設定更新

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,37 +2,42 @@ import js from "@eslint/js";
 import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
 
 export default tseslint.config([
   globalIgnores(["dist"]),
+
   {
     files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      react.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      parser: tseslint.parser,
+      parserOptions: {
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
     plugins: {
       react,
+      "react-hooks": reactHooks,
     },
     settings: {
       react: {
         version: "detect",
       },
     },
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true, // ReactのJSXを許可
-        },
-      },
+    rules: {
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
+
+  js.configs.recommended,
+  tseslint.configs.recommended,
 ]);
