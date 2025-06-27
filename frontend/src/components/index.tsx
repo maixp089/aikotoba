a33f8f13f807f767c20c560c4fcd81b6047ed61e
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-06-27 11:25:36 +0900

    add: index追加

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -3,4 +3,6 @@ export { default as BackToMyPage } from "./BackToMyPage";
 export { default as ToPresentation } from "./ToPresentation";
 export { default as ToRanking } from "./ToRanking";
 export { default as ToRecord } from "./ToRecord";
+export { default as ToNewAccount } from "./ToNewAccount";
 export { default as ToEvaluation } from "./ToEvaluation";
+export { default as ToLogin } from "./ToLogin";
