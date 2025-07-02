5e48d20b37386401091132bee5520ba5e1e0f7fc
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-07-02 09:11:28 +0900

    update: BackTo Mypageボタン配置変更

--- frontend/src/pages/Presentation.tsx
+++ frontend/src/pages/Presentation.tsx
@@ -1,11 +1,15 @@
 import { BackToMyPage, ToEvaluation, Layout } from "../components";
 import "../App.css";
 
+const images = ["/images/roco2.png", "/images/robo2.png"];
+
 const Presentation = () => {
   return (
     <Layout>
       <div className="space-y-10">
+        <BackToMyPage />
         <h1 className=" text-5xl text-center ">プレゼンページ</h1>
+
         <BackToMyPage />
         <ToEvaluation />
       </div>
