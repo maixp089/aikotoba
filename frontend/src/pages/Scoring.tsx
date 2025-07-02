17486d852ca7f204ca5526d7a5cada6c72a310b4
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-07-02 16:09:09 +0900

    add: APIレス待機ページ追加

---
+++ frontend/src/pages/Scoring.tsx
@@ -0,0 +1,13 @@
+import { Layout } from "../components";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="space-y-10">
+        <p className="text-5xl text-red-600">〇○.まるつけちゅ.○〇</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
