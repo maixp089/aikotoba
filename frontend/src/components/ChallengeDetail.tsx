5ac9a81f31c414008f12396d98b9b9ef23bb50e6
Tomomi <jesk.saeki.tomomi@gmail.com>
2025-07-03 13:32:40 +0900

    update: チャレンジコンポーネント文字サイズ、コンポーネントサイズ、色

--- frontend/src/components/ChallengeDetail.tsx
+++ frontend/src/components/ChallengeDetail.tsx
@@ -6,7 +6,7 @@ const ChallengeDetail = () => {
   const { datetime } = useParams(); // 例: "2025-06-30T09:15"
 
   const record = mockScoreAdviceData.find(
-    (entry) => `${entry.date}T${entry.time}` === datetime,
+    (entry) => `${entry.date}T${entry.time}` === datetime
   );
 
   if (!record) {
@@ -22,15 +22,18 @@ const ChallengeDetail = () => {
   return (
     <div
       className="
+    w-80
     text-2xl
     text-indigo-400 
     rounded-b-sm 
     text-center 
     shadow 
-    shadow-blue-200"
+    shadow-blue-200
+    bg-blue-100 
+    pt-2 px-5 py-2"
     >
       もっとチャレンジ！
-      <p className="text-gray-700 text-xs">{record.advice}</p>
+      <p className="text-gray-700 text-[14px]">{record.advice}</p>
     </div>
   );
 };
