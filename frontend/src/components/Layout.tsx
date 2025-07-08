ee98585c1c1369d20ee202cc45e7a807e10eb10c
えがしら0919 <@example.com>
2025-07-08 13:19:02 +0900

    fix:背景色削除

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -4,8 +4,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       style={{
         minHeight: "100vh",
         minWidth: "100vw", // 横幅いっぱい（必須ではないが推奨）
-        background: "linear-gradient(120deg,#d4efd7 0%, #f8f5e1 100%)",
-        fontFamily: "'M PLUS Rounded 1c', 'Kosugi Maru', 'sans-serif'",
         display: "flex",
         justifyContent: "center",   // ★これで「上下」も中央に
         alignItems: "center",       // ★これで「左右」中央
